7850a19064f4d209f73fcb4bad2ce7a54142239a
Maicol Rojas <maidanlrojas@gmail.com>
2024-05-13 11:08:43 -0500

    Agrega la configuracion de enrutamiento

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,32 @@
+/// Librerias de react
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+/// Paginas
+import { Home } from "./pages/home/Home";
+
+
+// Componente principal :
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
 
-    </>
+      <Routes>
+
+        {/* Path ruta por defecto */}
+        <Route path="/" element={<Navigate to="/inicio" />} />
+
+        {/* Path rutas */}
+        <Route path="inicio" element={<Home />} />
+
+        {/* Path para ruta no encontrada */}
+        <Route path="*" element={<Navigate to="/inicio" />} />
+
+      </Routes>
+
+    </BrowserRouter>
   )
+
 }
 
 export default App
