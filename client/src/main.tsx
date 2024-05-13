dc305c6a2ff3378886a6c6ffa5e373b3b0890d43
Maicol Rojas <maidanlrojas@gmail.com>
2024-05-13 10:50:30 -0500

    Actualiza Informacion App - Agrega estilos globales

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,13 @@
+/// Librerias de react
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+/// Componente principal
 import App from './App.tsx'
-import './index.css'
+
+/// Estilos
+import './styles/index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
